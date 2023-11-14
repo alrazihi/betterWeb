2f150656eed94c12b887ee179814ba99a19ab985
Alrazihi Towfik <tawfikalrazihi@gmail.com>
2023-11-14 17:37:05 +0100

    Setting the page title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', title: 'Home',component: HomeComponent },
   { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
 
 ];
