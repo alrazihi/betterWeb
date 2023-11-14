f5a20878c22faed42487f31f09137e94076c62ee
Alrazihi Towfik <tawfikalrazihi@gmail.com>
2023-11-14 17:35:19 +0100

    To display a 404 page, set up a wildcard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 // app.routes.ts
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  // Add more routes as needed
+  { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+
 ];
